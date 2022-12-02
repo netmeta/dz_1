b008c8ec87f31cfba07a97487a5c33a343483273
netmeta <moron686@gmail.com>
2022-12-02 17:06:24 +0300

    1

---
+++ a/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"max = {num1}");
+}
+else
+{
+    Console.WriteLine($"max = {num2}");
+}
---
+++ b/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите первое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > a) max = b;
+if (c > b) max = c;
+
+Console.WriteLine($"maxim = {max}");
